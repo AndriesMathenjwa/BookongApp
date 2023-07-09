caa78155d6cae4963799c79e8bc3f62a03c0aaa9
Andries Mathenjwa <andriesmathenjwa52@gmail.com>
2023-07-09 16:27:53 +0200

    Initial commit

---
+++ routes/hotels.js
@@ -0,0 +1,19 @@
+import express from "express"
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotelController.js";
+
+const router = express.Router();
+
+router.post("/", createHotel);  
+
+router.put("/:id",updatedHotel);
+
+router.delete("/:id",deleteHotel);
+
+router.get("/:id", getHotel);
+
+router.get("/",getHotels);
+
+
+export default router;
